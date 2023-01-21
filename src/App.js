872f871b123821e956590109c410b5070dfdd6b0
Swarzstein <zorrillagja@gmail.com>
2023-01-20 20:47:01 -0400

    Test for logic files and some components added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import './App.css';
-import React, { Component } from 'react';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <main className="container">
-          <Outlet />
-        </main>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Header />
+    <main className="container">
+      <Outlet />
+    </main>
+  </div>
+);
 
 export default App;
